255fb778e6abdf0d3d7f9dcfff9779041ac8ef96
DiegoAM97 <diegotar2@gmail.com>
2023-01-17 18:10:05 +0100

    Processing the prediction received from the API

--- WebApplication/react-frontend/src/components/Project.js
+++ WebApplication/react-frontend/src/components/Project.js
@@ -3,15 +3,32 @@ import React, { useState } from 'react'
 const Project = () => {
 
     const [selectedImage, setSelectedImage] = useState(null);
-    const [prediction, setprediction] = useState('');
+    const [predictionResult, setPredictionResult] = useState(null);
     const [errorMessage, setErrorMessage] = useState('');
+
+    const formData = new FormData();
+    formData.append('image', selectedImage);
+
+    let textPrediction;
+
+    if (predictionResult) {
+        if (predictionResult.prediction === "0") 
+            textPrediction = <div>No face mask used</div>
+        else if (predictionResult.prediction === "1")
+            textPrediction = <div>Face mask used correctly</div>
+        else if (predictionResult.prediction === "2")
+            textPrediction = <div>Face mask used incorrectly</div>
+    }
 
     const requestOptions = {
         method: 'POST',
-        headers: { 'Content-Type': 'multipart/form-data' },
         mode: 'cors',
-        body: null
+        body: formData
     };
+
+    const removePrediction = () => {
+        setPredictionResult(null);
+    }
 
     const removeErrorMessage = () => {
         setErrorMessage('');
@@ -19,26 +36,40 @@ const Project = () => {
 
     const updateImage = (e) => {
         setSelectedImage(e.target.files[0]);
+        removePrediction();
+        removeErrorMessage();
     }
 
     const predictImage = () => {
         fetch('http://localhost:8080/predict-image', requestOptions)
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => setPredictionResult(data))
             .catch(() => setErrorMessage('The browser sent a bad request, please check the input is correct.'))
     }
 
     return (
         <section id='Project' className='flex flex-col items-center p-10 bg-project'>
             {
-                errorMessage && 
-                <div className='bg-red-300 border-2 border-red-600 rounded-lg text-red-600 sm:text-xl text-sm sm:px-20 px-10'>
-                    {errorMessage}<span className='cursor-pointer relative sm:left-[4.5rem] left-[2rem] bottom-[.2rem]' onClick={removeErrorMessage}>ˣ</span>
+                predictionResult && 
+                <div className='flex bg-green-300 border-2 border-green-600 rounded-lg text-green-600 sm:text-xl text-sm text-center'>
+                    <div className='sm:px-16 px-10 py-2'>
+                        {textPrediction}
+                    </div>
+                    <span className='cursor-pointer pr-2' onClick={removePrediction}>ˣ</span>
                 </div>
             }
-            <div className='max-h-80 w-80'>
+            {
+                errorMessage && 
+                <div className='flex bg-red-300 border-2 border-red-600 rounded-lg text-red-600 sm:text-xl text-sm text-center'>
+                    <div className='sm:px-16 px-10 py-2'>
+                        {errorMessage}
+                    </div>
+                    <span className='cursor-pointer pr-2' onClick={removeErrorMessage}>ˣ</span>
+                </div>
+            }
+            <div>
                 {
-                    selectedImage && <img src={URL.createObjectURL(selectedImage)} alt='img-alt' className='my-4'></img>
+                    selectedImage && <img src={URL.createObjectURL(selectedImage)} alt='img-alt' className='my-4 max-h-80 max-w-40'></img>
                 }
             </div>
             <input onChange={updateImage} type='file' name='file' accept='.png, .jpg' className='border-4 rounded-lg my-2 p-2'></input>
